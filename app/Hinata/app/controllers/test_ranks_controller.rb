42e3ac905995953e5c7b6e046b765ba3db2ba113
ryo_hara_ <akstkr@gmail.com>
2018-08-13 05:49:29 +0900

    add: テストデータの追加とか外諸々

--- app/Hinata/app/controllers/test_ranks_controller.rb
+++ app/Hinata/app/controllers/test_ranks_controller.rb
@@ -8,12 +8,19 @@ class TestRanksController < ApplicationController
     def create
         json_request = JSON.parse(request.body.read)
         @get_value = json_request["Data"]["value"]
+        @get_score_id = json_request["Data"]["score_id"]
 
         #get_value = params.require(:Data).(:value)
+        id = get_scores_last_id + 1
 
-        @user = Score.new(id: 112, value: @get_value,created_at: Time.now,updated_at: Time.now)
+        @user = Score.new(id: id,score_id: @get_score_id , value: @get_value,created_at: Time.now,updated_at: Time.now)
         @user.save
 
     end
+
+    private
+        def get_scores_last_id
+            id = Score.order(:id).last.id
+        end
 
 end
--- app/Hinata/db/seeds.rb
+++ app/Hinata/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Score.create(:id => 0, :score_id => 9999,:value => 120,:created_at => '2018-08-12 14:30:03.821508',:updated_at => '2018-08-12 14:30:03.821508')
+Score.create(:id => 1, :score_id => 9999,:value => 200,:created_at => '2018-08-12 14:30:04.821508',:updated_at => '2018-08-12 14:30:04.821508')
+Score.create(:id => 2, :score_id => 9999,:value => 5,:created_at => '2018-08-12 14:30:05.821508',:updated_at => '2018-08-12 14:30:05.821508')
+Score.create(:id => 3, :score_id => 9999,:value => 60,:created_at => '2018-08-12 14:30:06.821508',:updated_at => '2018-08-12 14:30:06.821508')
